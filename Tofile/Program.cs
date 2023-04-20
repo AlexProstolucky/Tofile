7f420b94ddd2aae788bc56c0d50cac2b68919bab
AlexProstolucky <alexprostolucky@gmail.com>
2023-04-20 21:24:24 +0300

    Добавьте файлы проекта.

---
+++ Tofile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        using (FileStream potik = new FileStream("text.txt", FileMode.Append, FileAccess.Write))
+        {
+            Console.WriteLine("Введіть текст:");
+            string str = Console.ReadLine();
+            byte[] bytes = Encoding.Default.GetBytes(str);
+            potik.Write(bytes, 0, bytes.Length);
+            Console.WriteLine("Текст успішно записано!");
+        }
+    }
+}
